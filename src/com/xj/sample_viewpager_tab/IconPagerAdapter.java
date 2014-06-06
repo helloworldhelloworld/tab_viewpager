1447afb304391e3c9e56c1e001b1889da2cad75d
helloworldhelloworld <xiajie198912@gmail.com>
2014-06-06 08:39:10 +0800

    viewpager tab

---
+++ src/com/xj/sample_viewpager_tab/IconPagerAdapter.java
@@ -0,0 +1,11 @@
+package com.xj.sample_viewpager_tab;
+
+public interface IconPagerAdapter {
+    /**
+     * Get icon representing the page at {@code index} in the adapter.
+     */
+    int getIconResId(int index);
+
+    // From PagerAdapter
+    int getCount();
+}
